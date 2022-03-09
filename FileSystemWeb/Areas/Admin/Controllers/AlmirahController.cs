aeb0a143cd21d13370683759b6bb32af123c2adc
qutubhaider950@gmail.com <qutubhaider950@gmail.com>
2022-03-09 23:27:49 +0530

    Almirah and Shelves done

---
+++ FileSystemWeb/Areas/Admin/Controllers/AlmirahController.cs
@@ -0,0 +1,27 @@
+﻿using FileSystemBAL.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileSystemWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AlmirahController : Controller
+    {
+        private readonly IUnitOfWork moUnitOfWork;
+        private readonly static int miPageSize = 10;
+
+        public AlmirahController(IUnitOfWork foUnitOfWork)
+        {
+            moUnitOfWork = foUnitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View("~/Areas/Admin/Views/Almirah/AlmirahList.cshtml");
+        }
+
+        public IActionResult Detail()
+        {
+            return View("~/Areas/Admin/Views/Almirah/AlmirahDetail.cshtml");
+        }
+    }
+}
