74cdfc1df6bf680980fb979533d7248c2ef7b1ab
Vaibhav <svaibhavrnc@gmail.com>
2022-03-10 10:15:45 -0800

    Store List

--- FileSystemBAL/Store/Models/StoreListResult.cs
+++ FileSystemBAL/Store/Models/StoreListResult.cs
@@ -16,6 +16,5 @@ namespace FileSystemBAL.Store.Models
         public string stZoneName { get; set; }
         public string stDivisionName { get; set; }
         public string stDepartmentName { get; set; }
-        public string stDesignationName { get; set; }
     }
 }
