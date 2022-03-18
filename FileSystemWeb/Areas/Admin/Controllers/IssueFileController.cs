cf5202ddfc5e1b81a742ee48ee37b9a0a775cdeb
qutubhaider <qutubhaider950@gmail.com>
2022-03-18 11:21:29 +0530

    Issue File is done

---
+++ FileSystemWeb/Areas/Admin/Controllers/IssueFileController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FileSystemWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class IssueFileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Areas/Admin/Views/IssueFile/IssueFileList.cshtml");
+        }
+
+        public IActionResult Detail()
+        {
+            return View("~/Areas/Admin/Views/IssueFile/IssueFile.cshtml");
+        }
+    }
+}
+
