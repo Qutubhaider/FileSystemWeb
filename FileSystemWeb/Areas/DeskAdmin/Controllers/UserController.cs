3d4f6b19dd7d5c66b5fbbb40fda7be6eae9582fa
qutubhaider950@gmail.com <qutubhaider950@gmail.com>
2022-03-27 01:02:32 +0530

    Some issues fixed

--- FileSystemWeb/Areas/DeskAdmin/Controllers/UserController.cs
+++ FileSystemWeb/Areas/DeskAdmin/Controllers/UserController.cs
@@ -1,9 +1,11 @@
 ﻿using FileSystemBAL.Repository.IRepository;
 using FileSystemBAL.User.Models;
+using FileSystemUtility.Models;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 using static FileSystemUtility.Utilities.CommonConstant;
 
 namespace FileSystemWeb.Areas.DeskAdmin.Controllers
@@ -30,11 +32,18 @@ namespace FileSystemWeb.Areas.DeskAdmin.Controllers
             {
                 loUserProfile = moUnitOfWork.UserRepository.GetUserDetail(id);
             }
-            //loUserProfile.ZoneList = moUnitOfWork.ZoneRepository.GetZoneDropDown();
+            loUserProfile.DeskList = moUnitOfWork.DeskRepository.GetDeskDropDown(Convert.ToInt32(User.FindFirst(SessionConstant.DivisionId).Value.ToString()));
             loUserProfile.DepartmentList = moUnitOfWork.DepartmentRepository.GetDepartmentDropDown();
             loUserProfile.inZoneId = Convert.ToInt32(User.FindFirst(SessionConstant.ZoneId).Value.ToString());
             loUserProfile.inDivisionId= Convert.ToInt32(User.FindFirst(SessionConstant.DivisionId).Value.ToString());
             return View("~/Areas/DeskAdmin/Views/User/UserDetail.cshtml",loUserProfile);
+        }
+
+        public IActionResult GetDesignationDropDown(int fiDepartmentId)
+        {
+            List<Select2> DesignationDropDown = moUnitOfWork.DesignationRepository.GetDesignationDropDown(fiDepartmentId);
+            return Json(new { data = DesignationDropDown });
+
         }
     }
 }
