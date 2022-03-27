90db55e6772be7083a738e70d30092ea51c6c23b
qutubhaider950@gmail.com <qutubhaider950@gmail.com>
2022-03-27 10:15:49 +0530

    Assign File

---
+++ FileSystemWeb/Areas/DeskAdmin/Controllers/AssignFileController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileSystemWeb.Areas.DeskAdmin.Controllers
+{
+    public class AssignFileController : Controller
+    {
+        [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
+        [Area("DeskAdmin")]
+        public IActionResult Index()
+        {
+            return View("~/Areas/DeskAdmin/Views/User/AssignFileList.cshtml");
+        }
+    }
+}
