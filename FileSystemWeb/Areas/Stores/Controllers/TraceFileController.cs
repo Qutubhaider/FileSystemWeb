d3c956581d8f59fbfe038255eff2ed8c41791507
qutubhaider950@gmail.com <qutubhaider950@gmail.com>
2022-04-09 11:59:47 +0530

    Some change fixed  and new addon

---
+++ FileSystemWeb/Areas/Stores/Controllers/TraceFileController.cs
@@ -0,0 +1,30 @@
+﻿using FileSystemBAL.Repository.IRepository;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace FileSystemWeb.Areas.Stores.Controllers
+{
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
+    [Area("Stores")]
+    public class TraceFileController : Controller
+    {
+        private readonly IUnitOfWork moUnitOfWork;
+        private readonly static int miPageSize = 10;
+
+        public TraceFileController(IUnitOfWork foUnitOfWork)
+        {
+            moUnitOfWork = foUnitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View("~/Areas/Stores/Views/TraceFile/TraceFile.cshtml");
+        }
+    }
+}
---
+++ FileSystemWeb/Areas/Stores/Controllers/WithdrawFileController.cs
@@ -0,0 +1,29 @@
+﻿using FileSystemBAL.Repository.IRepository;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileSystemWeb.Areas.Stores.Controllers
+{
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
+    [Area("Stores")]
+    public class WithdrawFileController : Controller
+    {
+        private readonly IUnitOfWork moUnitOfWork;
+        private readonly static int miPageSize = 10;
+
+        public WithdrawFileController(IUnitOfWork foUnitOfWork)
+        {
+            moUnitOfWork = foUnitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View("~/Areas/Stores/Views/WithdrawFile/WithdrawFile.cshtml");
+        }
+    }
+}
