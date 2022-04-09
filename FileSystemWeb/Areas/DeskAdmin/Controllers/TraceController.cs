d0c7f240a5198099f717c8b6ab6bd19eff15263e
qutubhaider950@gmail.com <qutubhaider950@gmail.com>
2022-04-09 14:36:39 +0530

    Some Issue fixed

---
+++ FileSystemWeb/Areas/DeskAdmin/Controllers/TraceController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileSystemWeb.Areas.DeskAdmin.Controllers
+{
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
+    [Area("DeskAdmin")]
+    public class TraceController : Controller
+    {
+        public IActionResult TraceFile()
+        {
+            return View("~/Areas/DeskAdmin/Views/Trace/TraceFile.cshtml");
+        }
+
+        public IActionResult TraceCase()
+        {
+            return View("~/Areas/DeskAdmin/Views/Trace/TraceCase.cshtml");
+        }
+    }
+}
